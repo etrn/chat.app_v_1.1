1a3429435f0c787bf11b403f88ffeb7beaca04d3
Taras Pashko <taras.pashko@gmail.com>
2017-02-23 23:27:59 +0000

    badge added

---
+++ js/main.js
@@ -0,0 +1,6 @@
+document.getElementById('hider').onclick = function() {
+      document.getElementById('enter').style.display = 'none';
+    };
+document.getElementById('hide').onclick = function() {
+      document.getElementById('enter').style.display = 'none';
+    };
